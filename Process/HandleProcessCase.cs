46c06d9f1df668102ffbb9348879b787f115f093
deepak parashar <deepakparashar2005@gmail.com>
2016-02-19 23:12:03 +0530

    Changes in progress

--- Process/HandleProcessCase.cs
+++ Process/HandleProcessCase.cs
@@ -1,14 +1,31 @@
-﻿using System;
+﻿using Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomainModel;
 
 namespace Process
 {
     // This case handle the transactions with the bank.
     public class HandleProcessCase
     {
+        private static DateTimeOffset _lastProcessedDate = DateTimeOffset.MinValue;
+        public void SettleTransactions()
+        {
+            List<Block> blocksToProcess = new List<Block>(BlockChainMaster.BlockChain.Blocks.OfType<Block>());
+            if (_lastProcessedDate > DateTimeOffset.MinValue)
+            {
+                blocksToProcess = blocksToProcess.Where(b => b.TimeStamp > _lastProcessedDate).OrderBy(b => b.TimeStamp).ToList();
+            }
+            _lastProcessedDate = DateTimeOffset.Now;
+            foreach (Block block in blocksToProcess)
+            {
+                HandleGetCase objHandleGetCase = new HandleGetCase();
+
+            }
+        }
 
     }
 }
